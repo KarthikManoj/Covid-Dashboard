564377e73b02e7868b61b160d5a5067a17df0369
KarthikManoj <manojdeen12@gmail.com>
2020-07-03 00:08:42 +0530

    Updates

---
+++ precache-manifest.8119d64d8f2996bb40fb397142f252c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e30f0c18799e6ae5bbe3091de341c2a0",
+    "url": "/Covid-Dashboard/index.html"
+  },
+  {
+    "revision": "c99156a1cde76241950b",
+    "url": "/Covid-Dashboard/static/css/main.1eff298f.chunk.css"
+  },
+  {
+    "revision": "f0f8628159dbee56a16a",
+    "url": "/Covid-Dashboard/static/js/2.e2eb4bf2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-Dashboard/static/js/2.e2eb4bf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99156a1cde76241950b",
+    "url": "/Covid-Dashboard/static/js/main.1f6d6502.chunk.js"
+  },
+  {
+    "revision": "b697b80ee14cf638ddb7",
+    "url": "/Covid-Dashboard/static/js/runtime-main.69f34b05.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-Dashboard/static/media/covid.d7265326.png"
+  }
+]);
